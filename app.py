885e90deb9b1c3d52de4ed7144710959c04fadaf
manas_sontakke <sontakke.manas@gmail.com>
2026-03-07 05:28:10 +0530

    datatranformation_stable

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     logging.info("The execution of EndtoEnd has started.")
 
     try:
-        #data_ingestion = DataIngestion()
+        data_ingestion = DataIngestion()
         data_ingestion_config = DataIngestionConfig()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
         
-        data_transformation.initiate_data_transformation(train_path="artifacts/train.csv", test_path="artifacts/test.csv")
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     except Exception as e:
         logging.info("Custom exception has been raised.")
